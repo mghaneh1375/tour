71c9bc1fa29ea400aa311144e965ca44c8a06294
kiavash <kiavashbc@gmail.com>
2020-12-22 13:45:04 +0330

    debugs

---
+++ app/models/places/FoodMaterial.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\models\places;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FoodMaterial extends Model
+{
+    protected $guarded = [];
+    protected $table = 'foodMaterials';
+    public $timestamps = false;
+}
