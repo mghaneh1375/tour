1042445c2b95d43455b15cca4aee5c9f9aa5fc69
kiavash <kiavashbc@gmail.com>
2021-02-05 16:01:07 +0330

    some debug

--- app/Http/Middleware/ShareData.php
+++ app/Http/Middleware/ShareData.php
@@ -21,7 +21,7 @@ class ShareData
      */
     public function handle($request, Closure $next)
     {
-        $fileVersions = 165;
+        $fileVersions = 166;
 
         $config = \App\models\ConfigModel::first();
 
