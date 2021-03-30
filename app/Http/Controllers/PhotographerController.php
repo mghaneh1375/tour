5053cea5f991bcc2416d46cc8bb3385c6a703adc
kiavash <kiavashbc@gmail.com>
2021-03-30 12:12:14 +0430

    some debug in Photographer upload file

--- app/Http/Controllers/PhotographerController.php
+++ app/Http/Controllers/PhotographerController.php
@@ -9,6 +9,7 @@ use App\models\Reviews\ReviewPic;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 
 class PhotographerController extends Controller
 {
