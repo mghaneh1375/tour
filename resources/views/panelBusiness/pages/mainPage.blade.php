9f564eee100bdcef0938c0a4947c5150b38b413b
sina-ij <sinaijadi8888@yahoo.com>
2023-07-05 17:25:55 +0330

    error

--- resources/views/panelBusiness/pages/mainPage.blade.php
+++ resources/views/panelBusiness/pages/mainPage.blade.php
@@ -7,21 +7,22 @@
 
 @section('body')
     <style>
-        .mainPage{
+        .mainPage {}
 
-        }
-        .mainPage .headerTitle{
+        .mainPage .headerTitle {
             font-size: 22px;
             font-weight: bold;
             margin-bottom: 15px;
             padding-bottom: 5px;
             border-bottom: solid 1px #cfcfcf;
         }
-        .mainPage .cardsSection{
+
+        .mainPage .cardsSection {
             display: flex;
             align-items: center;
         }
-        .mainPage .cards{
+
+        .mainPage .cards {
             background: var(--koochita-light-green);
             color: black;
             height: 100px;
@@ -33,7 +34,8 @@
             margin-left: 22px;
             padding: 0px 25px;
         }
-        .mainPage .cards .icon{
+
+        .mainPage .cards .icon {
             width: 60px;
             height: 60px;
             display: flex;
@@ -44,17 +46,19 @@
             font-size: 35px;
             margin-left: 15px;
         }
-        .mainPage .cards .infoSec{
 
-        }
-        .mainPage .cards .infoSec .text{
+        .mainPage .cards .infoSec {}
+
+        .mainPage .cards .infoSec .text {
             font-weight: bold;
             font-size: 20px;
         }
-        .mainPage .cards .infoSec .numSec{
+
+        .mainPage .cards .infoSec .numSec {
             margin-top: 5px;
         }
-        .mainPage .cards .infoSec .numSec .num{
+
+        .mainPage .cards .infoSec .numSec .num {
             width: 20px;
             height: 20px;
             background: red;
@@ -64,17 +68,18 @@
             border-radius: 50%;
             color: white;
         }
-        .mainPage .cards .infoSec .numSec .tex{
+
+        .mainPage .cards .infoSec .numSec .tex {
             font-size: 12px;
         }
     </style>
     <div class="row mainPage">
         <div class="col-md-12">
-            @if($newTicketCount > 0 || $newNotificationCount > 0)
+            @if ($newTicketCount > 0 || $newNotificationCount > 0)
                 <div class="mainBackWhiteBody">
                     <div class="cardsSection">
 
-                        @if($newTicketCount != 0)
+                        @if ($newTicketCount != 0)
                             <a href="#" class="cards">
                                 <div class="icon">
                                     <i class="fa-solid fa-headset"></i>
@@ -82,14 +87,14 @@
                                 <div class="infoSec">
                                     <div class="text">پشتیبانی</div>
                                     <div class="numSec">
-                                        <span class="num">{{$newTicketCount}}</span>
+                                        <span class="num">{{ $newTicketCount }}</span>
                                         <span class="tex">پیام جدید</span>
                                     </div>
                                 </div>
                             </a>
                         @endif
 
-                        @if($newNotificationCount != 0)
+                        @if ($newNotificationCount != 0)
                             <a href="#" class="cards">
                                 <div class="icon">
                                     <i class="fa-regular fa-bell-on"></i>
@@ -97,7 +102,7 @@
                                 <div class="infoSec">
                                     <div class="text">اعلانات</div>
                                     <div class="numSec">
-                                        <span class="num">{{$newNotificationCount}}</span>
+                                        <span class="num">{{ $newNotificationCount }}</span>
                                         <span class="tex">پیام جدید</span>
                                     </div>
                                 </div>
@@ -111,27 +116,38 @@
             <div class="mainBackWhiteBody" style="margin-top: 10px">
                 <div class="headerTitle">کسب و کارهای من</div>
                 <div class="cardsSection">
-
-                    @foreach($myBusiness as $mb)
-                        <a href="{{$mb->url}}" class="cards">
-                            <div class="icon">
-                                @if($mb->type == 'agency')
-                                    <i class="fa-light fa-plane-tail"></i>
-                                @endif
+                    @if (count($myBusiness) == 0)
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img style="max-width: 100%" src="{{ URL::asset('images/icons/newsrv0102.png') }}"
+                                    alt="">
                             </div>
-                            <div class="infoSec">
-                                <div class="text">{{$mb->name}}</div>
-    {{--                            <div class="numSec">--}}
-    {{--                                <span class="num">{{$newTicketCount}}</span>--}}
-    {{--                                <span class="tex">پیام جدید</span>--}}
-    {{--                            </div>--}}
+                            <div class="col-md-7">
+                                <p>هیچ کسب و کاری موجود نیست. اولین کسب و کار خود را در کوچیتا ایجاد کنید.
+                                    اگر در ایجاد کسب و کار مشکلی دارید از قسمت پشتیبانی مشکل خود را با ما درمیان بگذارید.
+                                </p>
                             </div>
-                        </a>
-                    @endforeach
-
+                        </div>
+                    @else
+                        @foreach ($myBusiness as $mb)
+                            <a href="{{ $mb->url }}" class="cards">
+                                <div class="icon">
+                                    @if ($mb->type == 'agency')
+                                        <i class="fa-light fa-plane-tail"></i>
+                                    @endif
+                                </div>
+                                <div class="infoSec">
+                                    <div class="text">{{ $mb->name }}</div>
+                                    {{--                            <div class="numSec"> --}}
+                                    {{--                                <span class="num">{{$newTicketCount}}</span> --}}
+                                    {{--                                <span class="tex">پیام جدید</span> --}}
+                                    {{--                            </div> --}}
+                                </div>
+                            </a>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
     </div>
 @endsection
-
