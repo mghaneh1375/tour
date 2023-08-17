900bd1f52f13ce976471fc4f1d3bdb392ecdd349
mghaneh1375 <mghaneh1375@gmail.com>
2023-08-17 16:37:20 +0330

    nothing

--- app/Http/Controllers/FormCreator/AdminController.php
+++ app/Http/Controllers/FormCreator/AdminController.php
@@ -14,7 +14,7 @@ use Illuminate\Validation\Rule;
 class AdminController extends Controller {
 
     private static $KOOCHITA_SERVER = "https://koochita-server.bogenstudio.com/api/";
-    private static $ROOM_SERVER = "http://193.151.137.75/api/";
+    private static $ROOM_SERVER = "http://193.151.137.75:8081/api/";
 
     public function setPlaceId(Request $request, UserAsset $user_asset) {
 
