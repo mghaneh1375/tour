7d686d8edc14ce1920370679636ece775e301028
sina-ij <sinaijadi8888@yahoo.com>
2023-10-12 15:44:54 +0330

    cas

--- routes/businessPanelRoutes.php
+++ routes/businessPanelRoutes.php
@@ -36,7 +36,7 @@ Route::middleware(['BusinessPanelGuest', 'csrfVeri'])->group(function() {
 
     Route::get('/loginPage', function() {
         
-        return Redirect::to('https://tour.bogenstudio.com/login?redirectUrl=https://business.bogenstudio.com/login-callback&callback=https://business.bogenstudio.com/cas-auth');
+        return Redirect::to('https://tour.bogenstudio.com/cas/login?redirectUrl=https://business.bogenstudio.com/login-callback&callback=https://business.bogenstudio.com/cas-auth');
 
     })->name('businessPanel.loginPage');
 
